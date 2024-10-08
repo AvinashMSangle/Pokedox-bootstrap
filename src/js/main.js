083a641f27b9b38a349da84711102cd9043fb8e8
AvinashMSangle <avinashMS.3119@proton.me>
2024-10-08 08:12:17 +0000

    updated

--- src/js/main.js
+++ src/js/main.js
@@ -1,7 +1,8 @@
-import data from "./data";
+// Packages
 import shuffle from "array-shuffle";
+import Fuse from "fuse.js";
 
-// Component
+import data from "./data";
 import PokemonCard from "./components/PokemonCard";
 
 // === DOM Targeting ===
@@ -14,17 +15,46 @@ renderPokemon(shuffle(data));
 function renderPokemon(list) {
   dataRow.textContent = "";
 
+  if (!list.length) {
+    const pokemon = PokemonCard({
+      name: "Not found",
+      description: "Try another search",
+      image:
+        "https://i.pinimg.com/originals/f8/29/be/f829bed61f75627eea111dfde089fe2c.png ",
+    });
+    dataRow.appendChild(pokemon);
+  }
+
   list.forEach((pokemonObj) => {
-    PokemonCard(pokemonObj);
+    const pokemon = PokemonCard(pokemonObj);
+    dataRow.appendChild(pokemon);
   });
 }
 
+// Will be invoked on search
 function handleSearch(input) {
-  const filteredPokemon = data.filter((pokemonObj) =>
-    pokemonObj.name.toLowerCase().includes(input)
-  );
+  // const filteredPokemon = data.filter((pokemonObj) => {
+  //   return pokemonObj.name.toLowerCase().includes(input);
+  // });
 
-  renderPokemon(filteredPokemon);
+  // Create fuse object
+  const options = {
+    keys: ["name", "abilities"],
+    threshold: 0.5,
+  };
+  const fuse = new Fuse(data, options);
+
+  // Perform search
+  function performSearch() {
+    if (!input) return data;
+
+    const searched = fuse.search(input);
+    return searched.map((obj) => obj.item);
+  }
+
+  // Create without the 'item' key from fuse search
+  const filterdPokemon = performSearch();
+  renderPokemon(filterdPokemon);
 }
 
 inputEl.addEventListener("input", (e) => {
